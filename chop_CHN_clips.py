f64c787a74c47c36211879b91a67a5ac823f3c0a
carissa ott <126111005+rissaott@users.noreply.github.com>
2025-02-12 15:16:25 -0800

    Add files via upload

---
+++ chop_CHN_clips.py
@@ -0,0 +1,108 @@
+from pydub import AudioSegment
+import os
+
+
+#{audio}_{chn clip}_segment_{segment id}
+
+def chop_audio(child_id, input_file, output_dir, metadata_file):
+    # Load the audio file
+    audio = AudioSegment.from_file(input_file)
+    
+    # Define the chunk length
+    chunk_length_ms = 500
+    
+    # Prepare to store segments
+    segments = []
+
+    clip_id = input_file.split("/")[-1].split("_")[1]
+    print("clip id",clip_id)
+    
+    # Chop the audio into 500 ms bits
+    for start in range(0, len(audio), chunk_length_ms):
+        end = start + chunk_length_ms
+        segment = audio[start:end]
+        segments.append(segment)
+
+    # Check for remainder
+    remainder_start = len(audio) // chunk_length_ms * chunk_length_ms
+    remainder = audio[remainder_start:]
+    print(len(remainder))
+
+    if len(remainder) > 0:
+        if len(remainder) < 200:
+            # Append to the last segment
+            segments[-2] += remainder    
+            segments.pop()  # Remove the last segment
+
+        os.makedirs(output_dir, exist_ok=True)
+
+        # Ensure metadata directory exists
+        metadata_dir = os.path.dirname(metadata_file)  # Get the directory of the metadata file
+        os.makedirs(metadata_dir, exist_ok=True)  # Create it if it doesn't exist
+
+        # Check if the metadata file exists
+        file_exists = os.path.isfile(metadata_file)
+        
+        with open(metadata_file, 'a') as meta_file:
+            if not file_exists:
+                meta_file.write("child_id, CHN_clip_id, segment_id, duration (ms), filename, corresponding_CHN_clip, \n")  # Write header only if file does not exist
+                                    
+            for i, segment in enumerate(segments):
+                segment_id = f"{i + 1}"
+                segment_file = f"{child_id}_{clip_id}_segment_{i + 1}"
+                segment_length = len(segment)
+                segment.export(os.path.join(output_dir, f"{child_id}_{clip_id}_segment_{segment_id}.wav"), format="wav")
+                corresponding_CHN_clip = f"{child_id}_{clip_id}"
+                
+                # Write segment info to the metadata file
+                meta_file.write(f"{child_id}, {clip_id}, {segment_id}, {segment_length}, {segment_file},{corresponding_CHN_clip}\n")
+
+if __name__ == "__main__":
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the path to the audio file
+
+    # box input directory for CHN clips
+    input_dir = "/Users/carissaott/Library/CloudStorage/Box-Box/box-group-lena-studies/student_projects/carissa/AVAproject/its_processing/" 
+    # box input directory for CHN info and output
+    working_dir = "/Users/carissaott/Library/CloudStorage/Box-Box/box-group-lena-studies/student_projects/carissa/AVAproject/its_processing/"
+    # directories for processed its files and chn timestamps on local machine
+    chn_dir = os.path.join(input_dir, "chn_clips/") 
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    count = 0
+
+    for folder in os.listdir(chn_dir):
+        folder_path = os.path.join(chn_dir, folder)
+        # Extract child ID from the CSV filename (e.g., "688LTP1" from "688LTP1_CHN_timestamps.csv")
+        child_id = folder_path.split("/")[-1].split("_")[0]
+        print(child_id)
+        # Skip hidden files like .DS_Store (common on macOS)
+        if folder.startswith("."):
+            print(f"Skipping hidden file/folder: {folder}")
+            continue
+
+        output_dir = os.path.join(current_directory, f"500ms_segments/{child_id}")
+        if os.path.exists(output_dir):
+            print(f"Skipping {child_id}: output directory already exists.")
+            continue  # Skip this child_id if output directory already exists
+
+        metadata_file_path = os.path.join(current_directory, f"500ms_segment_metadata/{child_id}_segment_metadata.csv")  # Define your metadata file path
+
+        # Iterate through the files in this folder
+        for clip in os.listdir(folder_path):
+            clip_path = os.path.join(folder_path, clip)
+            
+            # Check if the file is a .wav audio file
+            if clip.endswith(".wav") and os.path.isfile(clip_path):
+                count += 1
+                #print(f"Found audio file: {clip_path}")
+
+                
+                chop_audio(child_id, clip_path, output_dir, metadata_file_path)
+                print(f"Chopping CHN clip: {clip}")
+
+        # After processing the folder, add child_id to the processed list
+        print(f"Completed processing for child_id: {child_id}. Added to processed list.")
+
+
---
+++ get_CHN_clips.py
@@ -0,0 +1,123 @@
+import re
+import sys
+import os
+from pydub import AudioSegment
+import random
+import pandas as pd
+import datetime
+from lxml import etree
+import numpy as np
+
+def load_audio(audio_file):
+    init = datetime.datetime.now()
+    print("loading audio")
+    fullAudio = AudioSegment.from_wav(audio_file)
+    print("audio for", os.path.basename(audio_file), "loaded in", datetime.datetime.now()-init, "min.sec.ms")
+    return fullAudio
+
+#_______________________________________________________________________________
+def extract_first_5_CHN_clips(csv_file, audio):
+    # Extract child ID from the CSV filename
+    child_id = os.path.splitext(os.path.basename(csv_file))[0].split('_')[0]
+    print(child_id)
+
+    # Create output directory
+    output_dir = os.path.join(working_dir,f"chn_clips/{child_id}_CHN_clips")
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Read the CSV file
+    df = pd.read_csv(csv_file)
+
+    for index, row in df.iterrows():
+        if row['segment_type'] == 'CHN':
+            # Get the clip onset and offset times
+            clip_onset = row['clip_onset'] * 1000  # Convert to milliseconds
+            clip_offset = row['clip_offset'] * 1000  # Convert to milliseconds
+            
+            # Extract the segment
+            extracted_clip = audio[clip_onset:clip_offset]
+
+            # Create the output filename
+            output_filename = f"{output_dir}/{child_id}_{row['seg_id']}_CHN_clip.wav"
+            extracted_clip.export(output_filename, format="wav")
+            print(f"Extracted: {output_filename}")
+
+#_______________________________________________________________________________
+def extract_audio_clips(csv_file, audio_file):
+    # Extract child ID from the CSV filename
+    # This identifies the recording, so same children at different time points are looked at as unique
+    child_id = os.path.splitext(os.path.basename(csv_file))[0].split('_')[0]
+    
+    # Create output directory
+    output_dir = os.path.join(working_dir,f"chn_clips/{child_id}_CHN_clips")
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Read the CSV file
+    df = pd.read_csv(csv_file)
+
+    # Load the audio file
+    audio = AudioSegment.from_file(audio_file)
+
+    for index, row in df.iterrows():
+        if row['segment_type'] == 'CHN':
+            # Get the clip onset and offset times
+            clip_onset = row['clip_onset'] * 1000  # Convert to milliseconds
+            clip_offset = row['clip_offset'] * 1000  # Convert to milliseconds
+            
+            # Extract the segment
+            extracted_clip = audio[clip_onset:clip_offset]
+
+            # Create the output filename
+            output_filename = f"{output_dir}/{child_id}_{row['seg_id']}_CHN_clip.wav"
+            extracted_clip.export(output_filename, format="wav")
+            print(f"Extracted: {output_filename}")
+
+#_______________________________________________________________________________
+if __name__ == "__main__":
+    # Get the directory of the current script
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    
+    # box input directory for raw audio files
+    input_dir = "/Users/carissaott/Library/CloudStorage/Box-Box/box-group-lena-studies/Soundscape/wavFiles/" 
+    # box input directory for CHN info and output
+    working_dir = "/Users/carissaott/Library/CloudStorage/Box-Box/box-group-lena-studies/student_projects/carissa/AVAproject/its_processing/"
+    # directories for processed its files and chn timestamps on local machine
+    chn_dir = working_dir+"chn_timestamp_metadata/"
+    count = 0
+
+    processed_child_ids = ["636LTP1", "018LTP2"]
+    for csv_file in os.listdir(chn_dir):
+        if csv_file.endswith("_CHN_timestamps.csv"):
+            # Extract child ID from the CSV filename (e.g., "688LTP1" from "688LTP1_CHN_timestamps.csv")
+            child_id = csv_file.split("_")[0]
+            count += 1
+            print(child_id)
+            csv_file_path = os.path.join(chn_dir, csv_file)
+            audio_file_path = os.path.join(input_dir, f"{child_id}.wav")
+
+            print(audio_file_path)
+
+            if os.path.exists(audio_file_path):
+        
+                # Skip this folder if it has already been processed
+                if child_id in processed_child_ids:
+                    print(f"Skipping already processed child_id: {child_id}")
+                    continue
+                    
+                # load raw audio
+                print("loading audio for " + child_id + "...")
+                audio = load_audio(audio_file_path)
+
+
+                # create 500 ms wav chunks
+                extract_first_5_CHN_clips(csv_file_path,audio)    
+                processed_child_ids.append(child_id) # append to processed_files list after processing
+
+                # Display the first few rows of the DataFrame to ensure it's the right file
+                print(f"Processing child ID: {child_id}")
+                print(f"CSV File: {csv_file_path}")
+                print(f"Audio File: {audio_file_path}")
+                
+            else:
+                print("ERROR: audio file does not exist for " + child_id)
+            
